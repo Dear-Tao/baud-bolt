c6b35ba0afaab4e77aa1557823328ab052c0e844
Tao <zhoutao@hhbpay.com>
2025-08-14 14:04:21 +0800

    yh

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,8 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   // GitHub Pages部署配置
-  // base: process.env.NODE_ENV === 'production' ? '/baud-bolt/' : '/',
-  base: '/baud-bolt/',
+  base: process.env.NODE_ENV === 'production' ? '/baud-bolt/' : '/',
+  // base: '/baud-bolt/',
   server: {
     port: 3000,
     open: true
